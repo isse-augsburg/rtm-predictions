60c4b6c80d5a3501b5169adfe016d02d5ce5f5f5
schroeni <niklas.schroeter@student.uni-augsburg.de>
2019-04-15 12:18:33 +0200

    datapipeline for image Reps

--- data_loaders.py
+++ data_loaders.py
@@ -1,145 +1,218 @@
 import h5py 
 import numpy as np 
+from PIL import Image
+from os import listdir, walk
+from os.path import isdir
 
+#returns a sequence of simulation steps as data and the filling percentage of the last step as label 
+def get_index_sequence(filename): 
+    indices = [10, 20, 30, 40, 50]
 
-class ERFH5_Filling_Factors_Loader():
-
-    def __init__(self):
-        print("Well Hello, Sir!")
-
-    #returns a sequence of simulation steps as data and the filling percentage of the last step as label 
-    def get_index_sequence(self, filename): 
-        indices = [10, 20, 30, 40, 50]
-
-        f = h5py.File(filename, 'r')
-        # Cut off last column (z), since it is filled with 1s anyway
-        all_states = f['post']['singlestate']
-        filling_factors_at_certain_times = list()
-        
-        for state in all_states:
-            try:
-                filling_factor =  f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['res'][()]
-                
-            except KeyError as e:
-                continue
-            
-            filling_factors_at_certain_times.append(filling_factor)   
-
-        flat_fillings = [x.flatten() for x in filling_factors_at_certain_times]
-        last_filling = flat_fillings[-1]
-        non_zeros = np.count_nonzero(last_filling)
-        state_count = np.shape(last_filling)[0]
-        filling_percentage = np.array(non_zeros/state_count)
-         
-        try:
-            flat_fillings = [flat_fillings[j] for j in indices]
-        except IndexError as ie:
-            return None 
-        
-        return [(flat_fillings, filling_percentage)]
+    f = h5py.File(filename, 'r')
+    # Cut off last column (z), since it is filled with 1s anyway
+    all_states = f['post']['singlestate']
+    filling_factors_at_certain_times = list()
     
-    def get_all_sequences_for_file(self,filename):
-        all_sequences = list()
-        t_begin = 0
-        t_end = 10
-        t_delta = 5 
-        t_target_offset = 1
-        t_sequence_distance = 10
-        t_final = 1000
+    for state in all_states:
+        try:
+            filling_factor =  f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['res'][()]
+            
+        except KeyError:
+            continue
         
-        while t_end + t_target_offset < t_final:
-            try:
-                instance = self.__get_fillings_at_times(filename, t_begin,t_end,t_delta,t_end+t_target_offset)
-            except Exception:
+        filling_factors_at_certain_times.append(filling_factor)   
+
+    flat_fillings = [x.flatten() for x in filling_factors_at_certain_times]
+    last_filling = flat_fillings[-1]
+    non_zeros = np.count_nonzero(last_filling)
+    state_count = np.shape(last_filling)[0]
+    filling_percentage = np.array(non_zeros/state_count)
+        
+    try:
+        flat_fillings = [flat_fillings[j] for j in indices]
+    except IndexError:
+        return None 
+    
+    return [(flat_fillings, filling_percentage)]
+
+def get_all_sequences_for_file(filename):
+    all_sequences = list()
+    t_begin = 0
+    t_end = 10
+    t_delta = 5 
+    t_target_offset = 1
+    t_sequence_distance = 10
+    t_final = 1000
+    
+    while t_end + t_target_offset < t_final:
+        try:
+            instance = __get_fillings_at_times(filename, t_begin,t_end,t_delta,t_end+t_target_offset)
+        except Exception:
+            break
+        
+        t_begin = t_begin +t_sequence_distance
+        t_end = t_end + t_sequence_distance
+
+        if instance is None: 
+            continue 
+        else: 
+            all_sequences.append(instance)
+
+    
+    if len(all_sequences) == 0:
+        return None 
+
+    return all_sequences
+
+
+def __get_fillings_at_times(filename, t_start, t_finish, t_delta, t_target):
+    t_now = t_start
+
+    try:
+        f = h5py.File(filename, 'r')
+    except OSError:
+        print(">>> ERROR: FILE", filename, "COULD NOT BE OPEND BY H5PY. THIS IS BAD. BIG OOooOF")
+        return None 
+
+    all_states = f['post']['singlestate']
+    filling_factors_at_certain_times = list()
+    filling_percentage = -1
+
+    final_state = ""
+    for s in all_states:
+        final_state = s 
+
+    end_time = f['post']['singlestate'][final_state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['indexval'][()]
+    if(end_time < t_target):
+        raise Exception
+
+    for state in all_states:
+        try:
+            time = f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['indexval'][()]
+            
+            if time >= t_target:
+                target_fillingstate = filling_factor =  f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['res'][()]
+                non_zeros = np.count_nonzero(target_fillingstate)
+                state_count = np.shape(target_fillingstate)[0]
+                filling_percentage = np.array(non_zeros/state_count)
+                t_target = 9999999
                 break
-            
-            t_begin = t_begin +t_sequence_distance
-            t_end = t_end + t_sequence_distance
-
-            if instance is None: 
-                continue 
-            else: 
-                all_sequences.append(instance)
-
-        
-        if len(all_sequences) == 0:
-            return None 
-
-        return all_sequences
-
-
-    def __get_fillings_at_times(self, filename, t_start, t_finish, t_delta, t_target):
-        t_now = t_start
-
-        try:
-            f = h5py.File(filename, 'r')
-        except OSError as E:
-            print(">>> ERROR: FILE", filename, "COULD NOT BE OPEND BY H5PY. THIS IS BAD. BIG OOooOF")
-            return None 
-
-        all_states = f['post']['singlestate']
-        filling_factors_at_certain_times = list()
-        filling_percentage = -1
-
-        final_state = ""
-        for s in all_states:
-            final_state = s 
-
-        end_time = f['post']['singlestate'][final_state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['indexval'][()]
-        if(end_time < t_target):
-            raise Exception
-    
-        for state in all_states:
-            try:
-                time = f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['indexval'][()]
-                
-                if time >= t_target:
-                    target_fillingstate = filling_factor =  f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['res'][()]
-                    non_zeros = np.count_nonzero(target_fillingstate)
-                    state_count = np.shape(target_fillingstate)[0]
-                    filling_percentage = np.array(non_zeros/state_count)
-                    t_target = 9999999
-                    break
-                if(time >= t_finish):
-                    continue
-                if(time >= t_now):
-                    filling_factor =  f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['res'][()]
-                    filling_factors_at_certain_times.append(filling_factor)   
-                    t_now += t_delta
-                
-            except KeyError as e:
+            if(time >= t_finish):
                 continue
-
-
-        if(t_target != 9999999 or filling_factors_at_certain_times.__len__() != (t_finish -t_start)/t_delta):
-            #print("Didn't",len(filling_factors_at_certain_times), t_target, filling_percentage)
-            return None 
-
-
-        flat_fillings = [x.flatten() for x in filling_factors_at_certain_times]
-    
-        #print("Worked",len(filling_factors_at_certain_times), t_target, filling_percentage)
-        return (flat_fillings, filling_percentage)
-
-    def get_single_states_and_fillings(self, filename):
-        instances = []
-        f = h5py.File(filename, 'r')
-        all_states = f['post']['singlestate']
-        
-        filling_factors_at_certain_times = list()
-        
-        for state in all_states:
-            try:
+            if(time >= t_now):
                 filling_factor =  f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['res'][()]
-    
-            except KeyError as e:
-                continue
+                filling_factors_at_certain_times.append(filling_factor)   
+                t_now += t_delta
             
-            filling_factors_at_certain_times.append(filling_factor)   
+        except KeyError:
+            continue
 
-        flat_fillings = [x.flatten() for x in filling_factors_at_certain_times]
-        single_states = [(data, np.array(0.0)) for data in flat_fillings]
 
+    if(t_target != 9999999 or filling_factors_at_certain_times.__len__() != (t_finish -t_start)/t_delta):
+        #print("Didn't",len(filling_factors_at_certain_times), t_target, filling_percentage)
+        return None 
+
+
+    flat_fillings = [x.flatten() for x in filling_factors_at_certain_times]
+
+    #print("Worked",len(filling_factors_at_certain_times), t_target, filling_percentage)
+    return (flat_fillings, filling_percentage)
+
+def get_single_states_and_fillings(filename):
+    instances = []
+    f = h5py.File(filename, 'r')
+    all_states = f['post']['singlestate']
+    
+    filling_factors_at_certain_times = list()
+    
+    for state in all_states:
+        try:
+            filling_factor =  f['post']['singlestate'][state]['entityresults']['NODE']['FILLING_FACTOR']['ZONE1_set1']['erfblock']['res'][()]
+
+        except KeyError:
+            continue
         
-        return single_states
+        filling_factors_at_certain_times.append(filling_factor)   
+
+    flat_fillings = [x.flatten() for x in filling_factors_at_certain_times]
+    single_states = [(data, np.array(0.0)) for data in flat_fillings]
+
+    
+    return single_states
+    
+
+def get_image_state_sequence(folder, start_state=0, end_state=20, step=1, label_offset=1):
+    filelist = get_filelist_within_folder(folder)
+    if len(filelist) == 0:
+        return None
+    filelist.sort()
+    state_list = list()
+
+    f = 0
+
+   
+    c_state = start_state
+
+    label  =None
+
+    while(f < len(filelist)):
+        f_meta = filelist[f]
+        f_split = f_meta.split("/")[-1].split("_")
+        state_index = int(f_split[0])
+
+
+        if state_index <= c_state:
+            state_list.append(load_image(f_meta))
+        f+=1
+        c_state+=step
         
+        if(c_state > end_state):
+            f+=label_offset-1
+            if(f < len(filelist)):
+                f_meta = filelist[f]
+                label = load_image(f_meta)
+
+            break
+
+    if label is None:
+        return None
+    
+    data = np.stack(state_list) 
+    if np.shape(data)[0] !=end_state - start_state +1:
+        return None
+
+    return [(data, label)]
+    # load right files
+    #load label#
+    #return instance
+
+def load_image( f_name ) :
+    img = Image.open( f_name )
+    img.load()
+    data = np.asarray( img, dtype="float32" )
+    return data
+
+# Functions to gather data within a folder
+def get_filelist_within_folder(root_directory):
+    dataset_filenames = []
+    for (dirpath, _, filenames) in walk(root_directory):
+        if filenames: 
+            filenames = [dirpath + '/' + f for f in filenames]
+            dataset_filenames.extend(filenames)
+    return dataset_filenames  
+
+def get_folders_within_folder(root_directory):
+    for (dirpath, dirnames, _) in walk(root_directory):
+        return [dirpath+ '/' + f for f in dirnames]
+
+
+if __name__ == "__main__":
+    folders = get_folders_within_folder('/cfs/home/s/c/schroeni/Git/tu-kaiserslautern-data/Images/')
+    for folder in folders:
+        instances = get_image_state_sequence(folder)
+        if instances is None:
+            
+            print("None")
+        else:
+            print(np.shape(instances[0][0]))
+
--- new_erfh5_pipeline.py
+++ new_erfh5_pipeline.py
@@ -1,140 +1,134 @@
-from os import listdir, walk 
+from os import listdir, walk
+from data_loaders import get_all_sequences_for_file, get_index_sequence, get_single_states_and_fillings, get_filelist_within_folder, get_folders_within_folder, get_image_state_sequence
 import threading
-import time 
-import random 
+import time
+import random
 from enum import Enum
-import torch 
+import torch
 
 import data_loaders as loaders
 
+
 class Thread_Safe_List():
-    
+
     def __init__(self, max_length=-1):
         self.list = []
         self.lock = threading.Lock()
         self.max_length = max_length
         self.finished = False
-    
+
     # if called, the thread using this queue will commit suicide
     def kill(self):
         self.finished = True
 
-
     def __len__(self):
-        #self.lock.acquire()
+        # self.lock.acquire()
         length = len(self.list)
-        #self.lock.release()
+        # self.lock.release()
         return length
 
-    def put(self, element): 
+    def put(self, element):
 
-        while len(self.list) >= self.max_length and self.max_length != -1: 
+        while len(self.list) >= self.max_length and self.max_length != -1:
             time.sleep(0.1)
-        
+
         self.lock.acquire()
         self.list.append(element)
-        
-        self.lock.release()
-    
-    def put_batch(self, batch): 
 
-        while len(self.list) >= self.max_length and self.max_length != -1: 
+        self.lock.release()
+
+    def put_batch(self, batch):
+
+        while len(self.list) >= self.max_length and self.max_length != -1:
             time.sleep(0.1)
 
         self.lock.acquire()
         self.list.extend(batch)
         self.lock.release()
 
-    def get(self, number_of_elements): 
-        
-        while len(self) < number_of_elements: 
+    def get(self, number_of_elements):
+
+        while len(self) < number_of_elements:
             if(self.finished):
                 raise StopIteration
             time.sleep(0.1)
-        
+
         self.lock.acquire()
         items = self.list[0:number_of_elements]
         self.list = self.list[number_of_elements:]
         self.lock.release()
-        return items 
+        return items
 
 
 class ERFH5_DataGenerator():
 
-    def __init__(self, data_path='/home/', data_function=None, batch_size=64,  epochs=80, max_queue_length=-1, num_validation_samples=1, num_workers=2):
+    def __init__(self, data_path='/home/', data_processing_function=None, data_gather_function=None, batch_size=64,  epochs=80, max_queue_length=-1, num_validation_samples=1, num_workers=2):
         self.data_path = data_path
         self.batch_size = batch_size
         self.epochs = epochs
         self.max_queue_length = max_queue_length
         self.num_validation_samples = num_validation_samples
         self.num_workers = num_workers
-        self.data_function = data_function
+        self.data_function = data_processing_function
+        self.data_gather = data_gather_function
 
-        if self.data_function == None:
-            raise Exception("No data_function specified!")
-        
+        if self.data_function is None or self.data_gather is None:
+            raise Exception(
+                "No data processing or reading function specified!")
 
-        
         self.data_dict = dict()
-        self.paths = self.__get_paths_to_files(self.data_path) 
+        self.paths = self.data_gather(self.data_path)
 
+        
         self.batch_queue = Thread_Safe_List(max_length=self.max_queue_length)
         self.path_queue = Thread_Safe_List()
         self.validation_list = []
         self.barrier = threading.Barrier(self.num_workers)
+
+        self.__fill_validation_list()
 
         try:
             self.__fill_path_queue()
         except Exception as e:
             raise e
 
-        self.__fill_validation_list()
+
 
         for i in range(self.num_workers):
             t_batch = threading.Thread(target=self.__fill_batch_queue)
             t_batch.start()
 
-
-    def __get_paths_to_files(self, root_directory):
-        dataset_filenames = []
-        for (dirpath, dirnames, filenames) in walk(root_directory):
-            if filenames: 
-                filenames = [dirpath + '/' + f for f in filenames]
-                dataset_filenames.extend(filenames)
-        return dataset_filenames  
-
-    def __fill_path_queue(self): 
+    def __fill_path_queue(self):
         if len(self.paths) == 0:
             raise Exception("No file paths found")
-        
+
         for i in range(self.epochs):
             new_paths = self.paths
             random.shuffle(new_paths)
             self.path_queue.put_batch(new_paths)
 
-    def __fill_validation_list(self): 
-        while len(self.validation_list) < self.num_validation_samples: 
-            sample = self.paths[0]
+    def __fill_validation_list(self):
+        while len(self.validation_list) < self.num_validation_samples:
+
+            sample = self.paths[0]			
             self.paths = self.paths[1:]
             instance = self.data_function(sample)
-            
-            #data_function must return [(data, label) ... (data, label)]
-            if instance is None: 
+
+            # data_function must return [(data, label) ... (data, label)]
+            if instance is None:
                 continue
             else:
                 for i in instance:
                     self.validation_list.append(i)
-        
 
-    def __fill_batch_queue(self): 
-        
+    def __fill_batch_queue(self):
 
         while len(self.path_queue) > 0:
             try:
                 file = self.path_queue.get(1)[0]
             except StopIteration as si:
                 break
-            
+
             if(len(self.path_queue) == 0):
                 self.path_queue.kill()
 
@@ -146,7 +140,7 @@ class ERFH5_DataGenerator():
                 self.batch_queue.put_batch(instance)
 
             else:
-                #data_function must return [(data, label) ... (data, label)]
+                # data_function must return [(data, label) ... (data, label)]
                 instance = self.data_function(file)
 
                 if instance is None:
@@ -156,47 +150,45 @@ class ERFH5_DataGenerator():
                     tensor_instances = list()
 
                     for i in instance:
-                        data, label = torch.FloatTensor(i[0]), torch.FloatTensor(i[1])
+                        data, label = torch.FloatTensor(
+                            i[0]), torch.FloatTensor(i[1])
                         self.batch_queue.put((data, label))
-                        tensor_instances.append((data,label))
-                    
-                    self.data_dict[file] = tensor_instances
- 
+                        tensor_instances.append((data, label))
 
-        #TODO 
+                    self.data_dict[file] = tensor_instances
+
+        # TODO
         self.barrier.wait()
         self.batch_queue.kill()
         print(">>>INFO: Data loading complete. - SUCCESS")
 
     def __iter__(self):
-        return self 
+        return self
 
     def __next__(self):
         try:
             batch = self.batch_queue.get(self.batch_size)
         except StopIteration as e:
-            raise e 
-            
+            raise e
+
         data = [i[0]for i in batch]
         labels = [i[1] for i in batch]
         data = torch.stack(data)
         labels = torch.stack(labels)
         return data, labels
 
-    def __len__(self): 
+    def __len__(self):
         return self.epochs * len(self.paths)
 
-
     def get_validation_samples(self):
-        return self.validation_list 
-        
+        return self.validation_list
+
+
 if __name__ == "__main__":
-    ff_loader = loaders.ERFH5_Filling_Factors_Loader()
+
+    generator = ERFH5_DataGenerator(data_path= "/cfs/home/s/c/schroeni/Git/tu-kaiserslautern-data/Images",
+                                    batch_size=1, epochs=2, max_queue_length=16, data_processing_function=get_image_state_sequence, data_gather_function=get_folders_within_folder)
 
     
-    generator = ERFH5_DataGenerator(data_path='/run/user/1001/gvfs/smb-share:server=137.250.170.56,share=share/data/RTM/Lautern/clean_erfh5/', 
-    batch_size=1, epochs=2, max_queue_length=16, data_function=ff_loader.get_all_sequences_for_file)
-
     for data, label in generator:
         print(data.size(), label.size())
-    
