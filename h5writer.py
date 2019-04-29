17ff4267cdea49e230145bfb1ffd458df6b082c9
schroeni <niklas.schroeter@student.uni-augsburg.de>
2019-04-29 15:47:41 +0200

    updated h5writer

--- h5writer.py
+++ h5writer.py
@@ -16,7 +16,7 @@ def write_dict_to_Hdf5(f, d:dict):
                 grp = f[str(k)]
             
             write_dict_to_Hdf5(grp, v)
-        elif type(v) is list:
+        elif type(v) is list and type(v[0]) is dict:
             try:
                 f = f.create_group(str(k))
             except ValueError:
@@ -24,6 +24,8 @@ def write_dict_to_Hdf5(f, d:dict):
             wrapped = wrap_list(v)
             write_dict_to_Hdf5(f,wrapped)
         else:
+            if type(v) is str:
+                f.create_dataset(str(k), data = v)
             f.create_dataset(str(k), data =  np.array(v))
 
 
@@ -49,138 +51,158 @@ def dict_appendor(el, d):
 
 if __name__ == "__main__":
     d = {
-    "shapes": [
-        {
+    "output_frequency_type": 0,
+    "perturbation_factors": {
+        "General_Sigma": 0.001,
+        "Shapes": {
+            "Rectangles": {
+                "Num": 10,
+                "Fiber_Content": [
+                    -0.3,
+                    0.3
+                ]
+            },
+            "Circles": {
+                "Num": 10,
+                "Fiber_Content": [
+                    -0.3,
+                    0
+                ]
+            }
+        }
+    },
+    "shapes": [{
             "Rectangle": {
-                "fvc": -0.23501463680736057,
-                "height": 4.5,
-                "width": 2.75
+                "fvc": -0.29499650602394134,
+                "height": 4.75,
+                "width": 6.875
             }
         },
         {
             "Rectangle": {
-                "fvc": -0.1361909449575696,
-                "height": 3.375,
-                "width": 1.5
+                "fvc": -0.11555955416902425,
+                "height": 4.625,
+                "width": 5.25
             }
         },
         {
             "Rectangle": {
-                "fvc": -0.10912324096799914,
-                "height": 7.125,
-                "width": 2.875
+                "fvc": 0.10998466945759067,
+                "height": 6.625,
+                "width": 6.125
             }
         },
         {
             "Rectangle": {
-                "fvc": -0.03555376111288022,
-                "height": 7.875,
-                "width": 3.375
-            }
-        },
-        {
-            "Rectangle": {
-                "fvc": 0.1691010838914181,
-                "height": 5.375,
-                "width": 5.375
-            }
-        },
-        {
-            "Rectangle": {
-                "fvc": 0.10456716763032725,
-                "height": 2.25,
-                "width": 6.5
-            }
-        },
-        {
-            "Rectangle": {
-                "fvc": -0.017124812014985502,
-                "height": 3.5,
+                "fvc": 0.24736346628701705,
+                "height": 5.25,
                 "width": 2.375
             }
         },
         {
             "Rectangle": {
-                "fvc": 0.12466440618953961,
-                "height": 6.75,
+                "fvc": -0.19367454313601146,
+                "height": 2.375,
+                "width": 6.875
+            }
+        },
+        {
+            "Rectangle": {
+                "fvc": -0.1039589830843318,
+                "height": 2.375,
+                "width": 5.25
+            }
+        },
+        {
+            "Rectangle": {
+                "fvc": -0.2460573018344987,
+                "height": 3.875,
+                "width": 1.125
+            }
+        },
+        {
+            "Rectangle": {
+                "fvc": 0.2916691508381806,
+                "height": 6.25,
                 "width": 6.0
             }
         },
         {
             "Rectangle": {
-                "fvc": 0.17095740214883742,
-                "height": 4.5,
-                "width": 4.0
+                "fvc": -0.12815623764975065,
+                "height": 7.625,
+                "width": 2.625
             }
         },
         {
             "Rectangle": {
-                "fvc": 0.21031252562672303,
-                "height": 1.375,
-                "width": 2.125
+                "fvc": 0.009141529067154486,
+                "height": 5.125,
+                "width": 7.5
             }
         },
         {
             "Circle": {
-                "fvc": -0.23666634579863027,
+                "fvc": -0.23507349652462617,
+                "radius": 1.625
+            }
+        },
+        {
+            "Circle": {
+                "fvc": -0.19819836709287625,
+                "radius": 1.375
+            }
+        },
+        {
+            "Circle": {
+                "fvc": -0.21887138547611146,
+                "radius": 1.375
+            }
+        },
+        {
+            "Circle": {
+                "fvc": -0.2381771208260994,
+                "radius": 1.75
+            }
+        },
+        {
+            "Circle": {
+                "fvc": -0.0894160696915535,
                 "radius": 2.75
             }
         },
         {
             "Circle": {
-                "fvc": -0.2988952652804413,
+                "fvc": -0.04709325030942252,
+                "radius": 2.375
+            }
+        },
+        {
+            "Circle": {
+                "fvc": -0.2638248085877042,
+                "radius": 1.75
+            }
+        },
+        {
+            "Circle": {
+                "fvc": -0.037285027564015194,
+                "radius": 1.5
+            }
+        },
+        {
+            "Circle": {
+                "fvc": -0.24862836339506822,
+                "radius": 2.75
+            }
+        },
+        {
+            "Circle": {
+                "fvc": -0.2381629990714681,
                 "radius": 1.875
-            }
-        },
-        {
-            "Circle": {
-                "fvc": -0.1185185560688713,
-                "radius": 2.0
-            }
-        },
-        {
-            "Circle": {
-                "fvc": -0.10014007510585546,
-                "radius": 1.375
-            }
-        },
-        {
-            "Circle": {
-                "fvc": -0.19697543443220716,
-                "radius": 2.25
-            }
-        },
-        {
-            "Circle": {
-                "fvc": -0.2702415878159245,
-                "radius": 2.625
-            }
-        },
-        {
-            "Circle": {
-                "fvc": -0.058009654067942146,
-                "radius": 2.5
-            }
-        },
-        {
-            "Circle": {
-                "fvc": -0.1356738699648369,
-                "radius": 1.125
-            }
-        },
-        {
-            "Circle": {
-                "fvc": -0.1351815688151721,
-                "radius": 2.125
-            }
-        },
-        {
-            "Circle": {
-                "fvc": -0.21838550137776358,
-                "radius": 1.375
             }
         }
     ]
 }
-    f = create_h5("test2")
+
+    f = create_h5("test8")
     write_dict_to_Hdf5(f, d)
