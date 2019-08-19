fixed wrong coordinate calculation in img data loader

Models/erfh5_DeconvModel.py
@@ -20,7 +20,7 @@ class DeconvModel(nn.Module):
         self.shaper = Conv2d(64, 64, 15, padding=7)  # 153
         self.med = Conv2d(64, 32, 7, padding=3)  # 153
         self.details = Conv2d(32, 16, 3)  # 151
-        self.details2 = Conv2d(16, 1, 3)  # 151
+        self.details2 = Conv2d(16, 1, 3, stride=2, padding=1)  # 151
 
     def forward(self, inputs):
         f = F.relu(self.fc(inputs))
@@ -36,4 +36,4 @@ class DeconvModel(nn.Module):
         t3 = F.relu(self.details(t2))
         t4 = torch.sigmoid(self.details2(t3))
         return torch.squeeze(t4, dim=1)
-#target will be 188/150
+

Pipeline/data_loaders_IMG.py
@@ -59,15 +59,20 @@ def get_image_state_sequence(folder, start_state=0, end_state=100, step=5, label
 
 
 def normalize_coords(coords):
-    max_c = np.max(coords)
-    min_c = np.min(coords)
-    coords = coords - min_c
-    coords = coords / (max_c - min_c)
+    coords = np.array(coords)
+    max_c = np.max(coords[:,0])
+    min_c = np.min(coords[:,0])
+    coords[:,0] = coords[:,0] - min_c
+    coords[:,0] = coords[:,0] / (max_c - min_c)
+    max_c = np.max(coords[:,1])
+    min_c = np.min(coords[:,1])
+    coords[:,1] = coords[:,1] - min_c
+    coords[:,1] = coords[:,1] / (max_c - min_c)
     return coords
 
 
     #for new data 37.5 and 30.0
-def create_np_image(target_shape=(293, 229), norm_coords=None, data=None, ):
+def create_np_image(target_shape=(148, 116), norm_coords=None, data=None, ):
     if norm_coords is None or data is None:
         print("ERROR")
         return