02da68ba7e7b51d5b1fa98e0dbaf1b9e6e045ff1
hartmade <Dennis.Hartmann@student.uni-augsburg.de>
2019-08-29 14:52:29 +0200

    Added x,y; check if parameters exist in HDF5-Object

---
+++ SQL_Like_Query/HDF5DB_Test.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class HDF5DBTest:
+    def test_save(self):
+        pass
+    
+    def test_load(self):
+        pass
+    
+    def test_addObjectsFromPath(self):
+        pass
+
+    def test_select(self):
+        pass
