8178eccf89874fe2f079234c1feaa0b1c5aba7b4
hartmade <Dennis.Hartmann@student.uni-augsburg.de>
2019-09-02 16:31:01 +0200

    Unittest continued, formatting and renaming, select with x and y

--- SQL_Like_Query/HDF5Object.py
+++ SQL_Like_Query/HDF5Object.py
@@ -10,123 +10,126 @@ from prettytable import PrettyTable
 
 
 class HDF5Object:
-    def __init__(self, metaPath, resultPath):
-        self.outputFrequencyTypePath = "output_frequency_type"
-        #Pertubations_Factors
-        self.generalSigmaPath = "perturbation_factors/General_Sigma"
-        #-Shapes
-        self.numberOfCirclesPath = "perturbation_factors/Shapes/Circles/Num"
-        self.numberOfRectanglesPath = "perturbation_factors/Shapes/Rectangles/Num"
-        self.numberOfRunnersPath = "perturbation_factors/Shapes/Runners/Num"
-        self.fibreContentCirclesPath = "perturbation_factors/Shapes/Circles/Fiber_Content"
-        self.fibreContentRectanglesPath = "perturbation_factors/Shapes/Rectangles/Fiber_Content"
-        self.fibreContentRunnersPath = "perturbation_factors/Shapes/Runners/Fiber_Content"
-        #Shapes
-        #-Circlepaths
-        self.fvcCirclePath = "shapes/Circle/fvc"
-        self.radiusCirclePath = "shapes/Circle/radius"
-        self.posXCirclePath = "shapes/Circle/posX"
-        self.posYCirclePath = "shapes/Circle/posY"
-        #-Rectanglepaths
-        self.fvcRectanglePath = "shapes/Rectangle/fvc"
-        self.heightRectanglePath = "shapes/Rectangle/height"
-        self.widthRectanglePath = "shapes/Rectangle/width"
-        self.posXRectanglePath = "shapes/Rectangle/posX"
-        self.posYRectanglePath = "shapes/Rectangle/posY"
-        #-Runnerspaths
-        self.fvcRunnerPath = "shapes/Runner/fvc"
-        self.heightRunnerPath = "shapes/Runner/height"
-        self.widthRunnerPath = "shapes/Runner/width"
-        self.posXRunnerPath = "shapes/Runner/posX"
-        self.posYRunnerPath = "shapes/Runner/posY"
-        self.posLowerLeftXRunnerPath = "shapes/Runner/posLowerLeftX"
-        self.posLowerLeftYRunnerPath = "shapes/Runner/posLowerLeftY"
+    def __init__(self, meta_path, result_path):
+        self.output_frequency_type_path =       "output_frequency_type"
+        # Pertubations_Factors
+        self.general_sigma_path =               "perturbation_factors/General_Sigma"
+        # -Shapes
+        self.number_of_circles_path =           "perturbation_factors/Shapes/Circles/Num"
+        self.number_of_rectangles_path =        "perturbation_factors/Shapes/Rectangles/Num"
+        self.number_of_runners_path =           "perturbation_factors/Shapes/Runners/Num"
+        self.fibre_content_circles_path =       "perturbation_factors/Shapes/Circles/Fiber_Content"
+        self.fibre_content_rectangles_path =    "perturbation_factors/Shapes/Rectangles/Fiber_Content"
+        self.fibre_content_runners_path =       "perturbation_factors/Shapes/Runners/Fiber_Content"
 
-        self.singleStatePath = "post/singlestate"
-        self.numberOfSensorsPath = "post/multistate/TIMESERIES1/multientityresults/SENSOR/PRESSURE/ZONE1_set1/erfblock/res"
+        # Shapes
+        # -Circlepaths
+        self.fvc_circle_path      = "shapes/Circle/fvc"
+        self.radius_circle_path   = "shapes/Circle/radius"
+        self.posx_circle_path     = "shapes/Circle/posX"
+        self.posy_circle_path     = "shapes/Circle/posY"
+        # -Rectanglepaths
+        self.fvc_rectangle_path =       "shapes/Rectangle/fvc"
+        self.height_rectangle_path =    "shapes/Rectangle/height"
+        self.width_rectangle_path =     "shapes/Rectangle/width"
+        self.posx_rectangle_path =      "shapes/Rectangle/posX"
+        self.posy_rectangle_path =      "shapes/Rectangle/posY"
+        # -Runnerspaths
+        self.fvc_runner_path =             "shapes/Runner/fvc"
+        self.height_runner_path =          "shapes/Runner/height"
+        self.width_runner_path =           "shapes/Runner/width"
+        self.posx_runner_path =            "shapes/Runner/posX"
+        self.posy_runner_path =            "shapes/Runner/posY"
+        self.pos_lower_leftx_runner_path = "shapes/Runner/posLowerLeftX"
+        self.pos_lower_lefty_runner_path = "shapes/Runner/posLowerLeftY"
 
-        self.numberOfCircles = 0
-        self.numberOfRectangles = 0
-        self.numberOfRunners = 0
-        self.numberOfSensors = 0
+        self.single_state_path =        "post/singlestate"
+        self.number_of_sensors_path =   "post/multistate/TIMESERIES1/multientityresults/SENSOR/PRESSURE/ZONE1_set1/erfblock/res"
 
-        #Metadata
-        m = h5py.File(metaPath, 'r')
-        self.metaPath = metaPath
-        if(self.outputFrequencyTypePath in m):
-            self.outputFrequencyType = m[self.outputFrequencyTypePath][()]
+        self.number_of_circles = 0
+        self.number_of_rectangles = 0
+        self.number_of_runners = 0
+        self.number_of_sensors = 0
 
-        if(self.generalSigmaPath in m):
-            self.generalSigma = m[self.generalSigmaPath][()]
-        if(self.numberOfCirclesPath in m):
-            self.numberOfCircles = m[self.numberOfCirclesPath][()]
-        if(self.numberOfRectanglesPath in m):
-            self.numberOfRectangles = m[self.numberOfRectanglesPath][()]
-        if(self.numberOfRunnersPath in m):
-            self.numberOfRunners = m[self.numberOfRunnersPath][()]
-        self.numberOfShapes = self.numberOfCircles + self.numberOfRectangles + self.numberOfRunners
-        if(self.fibreContentCirclesPath in m):
-            self.fibreContentCircles = m[self.fibreContentCirclesPath][()]
-        if(self.fibreContentRectanglesPath in m):
-            self.fibreContentRectangles = m[self.fibreContentRectanglesPath][()]
+        # Metadata
+        m = h5py.File(meta_path, 'r')
+        self.meta_path = meta_path
+        if(self.output_frequency_type_path in m):
+            self.output_frequency_type = m[self.output_frequency_type_path][()]
 
-        if(self.fibreContentRunnersPath in m):
-            self.fibreContentRunners = m[self.fibreContentRunnersPath][()]
+        if(self.general_sigma_path in m):
+            self.general_sigma = m[self.general_sigma_path][()]
+        if(self.number_of_circles_path in m):
+            self.number_of_circles = m[self.number_of_circles_path][()]
+        if(self.number_of_rectangles_path in m):
+            self.number_of_rectangles = m[self.number_of_rectangles_path][()]
+        if(self.number_of_runners_path in m):
+            self.number_of_runners = m[self.number_of_runners_path][()]
+        self.number_of_shapes = self.number_of_circles + self.number_of_rectangles + self.number_of_runners
+        if(self.fibre_content_circles_path in m):
+            self.fibre_content_circles = m[self.fibre_content_circles_path][()]
+        if(self.fibre_content_rectangles_path in m):
+            self.fibre_content_rectangles = m[self.fibre_content_rectangles_path][()]
 
-        #Shapes
-        #-Circle
-        if(self.fvcCirclePath in m):
-            self.fvcCircle = np.array(m[self.fvcCirclePath][()])
-        if(self.radiusCirclePath in m):
-            self.radiusCircle = np.array(m[self.radiusCirclePath][()])
-        if(self.posXCirclePath in m):
-            self.posXCircle = np.array(m[self.posXCirclePath][()])
-        if(self.posYCirclePath in m):
-            self.posyCircle = np.array(m[self.posYCirclePath][()])
-        #-Rectangle
-        if(self.fvcRectanglePath in m):
-            self.fvcRectangle = np.array(m[self.fvcRectanglePath][()])
-        if(self.heightRectanglePath in m):
-            self.heightRectangle = np.array(m[self.heightRectanglePath][()])
-        if(self.widthRectanglePath in m):
-            self.widthRectangle = np.array(m[self.widthRectanglePath][()])
-        if(self.posXRectanglePath in m):
-            self.posXRectangle = np.array(m[self.posXRectanglePath][()])
-        if(self.posYRectanglePath in m):
-            self.posYRectangle = np.array(m[self.posYRectanglePath][()])
-        #-Runner
-        if(self.fvcRunnerPath in m):
-            self.fvcRunner = np.array(m[self.fvcRunnerPath][()])
-        if(self.heightRunnerPath in m):
-            self.heightRunner = np.array(m[self.heightRunnerPath][()])
-        if(self.widthRunnerPath in m):
-            self.widthRunner = np.array(m[self.widthRunnerPath][()])
-        if(self.posXRunnerPath in m):
-            self.posXRunner = np.array(m[self.posXRunnerPath][()])
-        if(self.posYRunnerPath in m):
-            self.posYRunner = np.array(m[self.posYRunnerPath][()])
-        if(self.posLowerLeftXRunnerPath in m):
-            self.posLowerLeftXRunner = np.array(m[self.posLowerLeftXRunnerPath][()])
-        if(self.posLowerLeftYRunnerPath in m):
-            self.posLowerLeftYRunner = np.array(m[self.posLowerLeftYRunnerPath][()])
+        if(self.fibre_content_runners_path in m):
+            self.fibre_content_runners = m[self.fibre_content_runners_path][()]
+
+        # Shapes
+        # -Circle
+        if(self.fvc_circle_path in m):
+            self.fvc_circle = np.array(m[self.fvc_circle_path][()])
+        if(self.radius_circle_path in m):
+            self.radius_circle = np.array(m[self.radius_circle_path][()])
+        if(self.posx_circle_path in m):
+            self.posx_circle = np.array(m[self.posx_circle_path][()])
+        if(self.posy_circle_path in m):
+            self.posy_circle = np.array(m[self.posy_circle_path][()])
+        # -Rectangle
+        if(self.fvc_rectangle_path in m):
+            self.fvc_rectangle = np.array(m[self.fvc_rectangle_path][()])
+        if(self.height_rectangle_path in m):
+            self.height_rectangle = np.array(m[self.height_rectangle_path][()])
+        if(self.width_rectangle_path in m):
+            self.width_rectangle = np.array(m[self.width_rectangle_path][()])
+        if(self.posx_rectangle_path in m):
+            self.posx_rectangle = np.array(m[self.posx_rectangle_path][()])
+        if(self.posy_rectangle_path in m):
+            self.posy_rectangle = np.array(m[self.posy_rectangle_path][()])
+        # -Runner
+        if(self.fvc_runner_path in m):
+            self.fvc_runner = np.array(m[self.fvc_runner_path][()])
+        if(self.height_runner_path in m):
+            self.height_runner = np.array(m[self.height_runner_path][()])
+        if(self.width_runner_path in m):
+            self.width_runner = np.array(m[self.width_runner_path][()])
+        if(self.posx_runner_path in m):
+            self.posx_runner = np.array(m[self.posx_runner_path][()])
+        if(self.posy_runner_path in m):
+            self.posy_runner = np.array(m[self.posy_runner_path][()])
+        if(self.pos_lower_leftx_runner_path in m):
+            self.pos_lower_leftx_runner = np.array(m[self.pos_lower_leftx_runner_path][()])
+        if(self.pos_lower_lefty_runner_path in m):
+            self.pos_lower_lefty_runner = np.array(m[self.pos_lower_lefty_runner_path][()])
         m.close()
 
-        #Results
-        r = h5py.File(resultPath, 'r')
-        self.resultPath = resultPath
+        # Results
+        r = h5py.File(result_path, 'r')
+        self.result_path = result_path
 
         temp = ""
-        if(self.singleStatePath in r):
-            for key in r[self.singleStatePath].keys():
+        if(self.single_state_path in r):
+            for key in r[self.single_state_path].keys():
                 temp = key
-                while not (self.singleStatePath + "/" + temp + "/entityresults/NODE/FILLING_FACTOR/ZONE1_set1/erfblock/res" in r):
+                while not (self.single_state_path + "/" + temp + "/entityresults/NODE/FILLING_FACTOR/ZONE1_set1/erfblock/res" in r):
                     temp = self.decrement(temp)
             temp = r["post/singlestate/" + temp + "/entityresults/NODE/FILLING_FACTOR/ZONE1_set1/erfblock/res"][()]
-        self.avgLevel = np.sum(temp) / len(temp)
+        if(len(temp) > 0):
+            self.avg_level = np.sum(temp) / len(temp)
 
-        self.age = datetime.strptime(re.search("([0-9]{4}\-[0-9]{2}\-[0-9]{2}_[0-9]{2}\-[0-9]{2}\-[0-9]{2})", resultPath).group(1), '%Y-%m-%d_%H-%M-%S')
-        if(self.numberOfSensorsPath in r):
-            self.numberOfSensors = np.shape(r[self.numberOfSensorsPath][()])[1]
+        if(re.search("([0-9]{4}\-[0-9]{2}\-[0-9]{2}_[0-9]{2}\-[0-9]{2}\-[0-9]{2})", result_path) != None):
+            self.age = datetime.strptime(re.search("([0-9]{4}\-[0-9]{2}\-[0-9]{2}_[0-9]{2}\-[0-9]{2}\-[0-9]{2})", result_path).group(1), '%Y-%m-%d_%H-%M-%S')
+        if(self.number_of_sensors_path in r):
+            self.number_of_sensors = np.shape(r[self.number_of_sensors_path][()])[1]
         r.close()
 
     def decrement(self, s):
@@ -138,52 +141,56 @@ class HDF5Object:
             s = s[:max(end-len(next), start)] + next + s[end:]
         return s
 
-    def showObjectContent(self):
+    def show_object_content(self):
         x = PrettyTable()
         x.field_names = ["Metaparameters", "Metadata"]
-        x.add_row(["Path_Metadata", str(self.metaPath)])
-        if(hasattr(self, 'outputFrequencyType')):
-            x.add_row(["Output_Frequency_Type", str(self.outputFrequencyType)])
+        x.add_row(["Path_Metadata", str(self.meta_path)])
+        if(hasattr(self, 'output_frequency_type')):
+            x.add_row(["Output_Frequency_Type", str(self.output_frequency_type)])
 
-        if(hasattr(self, 'generalSigma')):
-            x.add_row(["General_Sigma", str(self.generalSigma)])
-        if(hasattr(self, 'numberOfCircles')):
-            x.add_row(["Number_Circles", str(self.numberOfCircles)])
-        if(hasattr(self, 'numberOfRectangles')):
-            x.add_row(["Number_Rectangles", str(self.numberOfRectangles)])
-        if(hasattr(self, 'numberOfRunners')):
-            x.add_row(["Number_Runners", str(self.numberOfRunners)])
-        if(hasattr(self, 'numberOfShapes')):
-            x.add_row(["Number_Shapes", str(self.numberOfShapes)])
-        if(hasattr(self, 'fibreContentCircles')):
-            x.add_row(["Fibre_Content_Circles", str(self.fibreContentCircles)])
-        if(hasattr(self, 'fibreContentRectangles')):
-            x.add_row(["Fibre_Content_Rectangles", str(self.fibreContentRectangles)])
-        if(hasattr(self, 'fibreContentRunners')):
-            x.add_row(["Fibre_Content_Runners", str(self.fibreContentRunners)])
+        if(hasattr(self, 'general_sigma')):
+            x.add_row(["General_Sigma", str(self.general_sigma)])
+        if(hasattr(self, 'number_of_circles')):
+            x.add_row(["Number_Circles", str(self.number_of_circles)])
+        if(hasattr(self, 'number_of_rectangles')):
+            x.add_row(["Number_Rectangles", str(self.number_of_rectangles)])
+        if(hasattr(self, 'number_of_runners')):
+            x.add_row(["Number_Runners", str(self.number_of_runners)])
+        if(hasattr(self, 'number_of_shapes')):
+            x.add_row(["Number_Shapes", str(self.number_of_shapes)])
+        if(hasattr(self, 'fibre_content_circles')):
+            x.add_row(["Fibre_Content_Circles", str(self.fibre_content_circles)])
+        if(hasattr(self, 'fibre_content_rectangles')):
+            x.add_row(["Fibre_Content_Rectangles", str(self.fibre_content_rectangles)])
+        if(hasattr(self, 'fibre_content_runners')):
+            x.add_row(["Fibre_Content_Runners", str(self.fibre_content_runners)])
 
-        if(hasattr(self, 'fvcCircle')):
-            x.add_row(["FVC_Circle", str(self.fvcCircle)])
-        if(hasattr(self, 'radiusCircle')):
-            x.add_row(["Radius_Circle", str(self.radiusCircle)])
-        if(hasattr(self, 'fvcRectangle')):
-            x.add_row(["FVC_Rectangle", str(self.fvcRectangle)])
-        if(hasattr(self, 'heightRectangle')):
-            x.add_row(["Height_Rectangle", str(self.heightRectangle)])
-        if(hasattr(self, 'widthRectangle')):
-            x.add_row(["Width_Rectangle", str(self.widthRectangle)])
-        if(hasattr(self, 'posXRectangle')):
-            x.add_row(["PosX_Rectangle", str(self.posXRectangle)])
-        
-        #Result
+        if(hasattr(self, 'fvc_circle')):
+            x.add_row(["FVC_Circle", str(self.fvc_circle)])
+        if(hasattr(self, 'radius_circle')):
+            x.add_row(["Radius_Circle", str(self.radius_circle)])
+        if(hasattr(self, 'fvc_rectangle')):
+            x.add_row(["FVC_Rectangle", str(self.fvc_rectangle)])
+        if(hasattr(self, 'height_rectangle')):
+            x.add_row(["Height_Rectangle", str(self.height_rectangle)])
+        if(hasattr(self, 'width_rectangle')):
+            x.add_row(["Width_Rectangle", str(self.width_rectangle)])
+        if(hasattr(self, 'posx_rectangle')):
+            x.add_row(["PosX_Rectangle", str(self.posx_rectangle)])
+
+        # Result
         y = PrettyTable()
         y.field_names = ["Resultparameters", "Resultdata"]
-        y.add_row(["     Path_Resultdata    ", str(self.resultPath) + "  "])        
-        y.add_row(["Average_Level", str(self.avgLevel)])
-        y.add_row(["Age of file", str(self.age)])
-        y.add_row(["Number_Sensors", str(self.numberOfSensors)])
+        if(hasattr(self, 'result_path')):
+            y.add_row(["     Path_Resultdata    ", str(self.result_path) + "  "]) 
+        if(hasattr(self, 'avg_level')):
+            y.add_row(["Average_Level", str(self.avg_level)])
+        if(hasattr(self, 'age')):
+            y.add_row(["Age of file", str(self.age)])
+        if(hasattr(self, 'number_of_sensors')):
+            y.add_row(["Number_Sensors", str(self.number_of_sensors)])
         print(x)
         print(y)
-    
-#test = HDF5Object("Data/2019-07-23_15-38-08_0")
-#test.showObjectContent()
+
+# test = HDF5Object("Data/2019-07-23_15-38-08_0")
+# test.showObjectContent()
--- SQL_Like_Query/Main.py
+++ SQL_Like_Query/Main.py
@@ -1,25 +1,25 @@
 from HDF5DB import HDF5DB
-from HDF5Object import HDF5Object
 
 
 def main():
   test = HDF5DB()
-  #test2 = HDF5Object("Data/0") #/2019-07-23_15-38-08_0
-  #test.addObject(test2)
-  test.addObjectsFromPath("Data")
-  #test.showObjects()
+  # test2 = HDF5Object("Data/0") #/2019-07-23_15-38-08_0
+  # test.addObject(test2)
+  test.add_objects_from_path("Data")
+  # test.showObjects()
   test.save("Data", "HDF5DB")
   test.load("Data", "HDF5DB")
-  test.showSelectionOptions()
-  #test.showObjects()
-  #test.select("age", "2019-07-23_15-38-05", ">")
-  #test.select("outputFrequencyType", 1, ">")
-  #test.select("fibreContentRunners", -0.7, "=")
-  #test.select("widthRectangle", 6, "<")
-  test.showObjects()
+  test.show_selection_options()
+  # test.showObjects()
+  # test.select("age", "2019-07-23_15-38-05", ">")
+  # test.select("output_frequency_type", 1, ">")
+  # test.select("fibre_content_runners", -0.7, "=")
+  test.select("avg_level", 0.8, ">")
+  test.show_objects()
 
 if(__name__ == "__main__"):
     main()
 
-#TODO:
+# TODO:
 #       Testklasse für HDF5DB sowie HDF5Object
+#       outputfrequency type speichern, evtl. in result, ansonsten simcreator ändern
