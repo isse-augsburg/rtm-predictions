960be465717043219ca524fc1f5fe75ee9f37a15
Simon Stieber <simon.stieber@student.uni-augsburg.de>
2019-09-18 13:41:04 +0200

    Ci testing

--- Tests/test_eval.py
+++ Tests/test_eval.py
@@ -3,6 +3,8 @@ import re
 import shutil
 import unittest
 
+import numpy as np
+
 import Tests.resources_for_testing as resources
 from model_trainer_sensor_to_flow import SensorTrainer
 
@@ -22,8 +24,8 @@ class TestEval(unittest.TestCase):
         self.expected_num_frames = self.num_test_samples * 3
         self.expected_num_epochs_during_training = 5
 
-    @unittest.skipIf(resources.running_in_docker(),
-                     "Skipped only on runner / in docker: not enough memory: currently not working")
+    # @unittest.skipIf(resources.running_in_docker(),
+    #                  "Skipped only on runner / in docker: not enough memory: currently not working")
     def test_eval(self):
         self.st = SensorTrainer(
             data_source_paths=[],
@@ -33,10 +35,8 @@ class TestEval(unittest.TestCase):
         self.st.inference_on_test_set(source_path=self.eval_path, output_path=self.eval_output_path)
         with open(self.eval_output_path / 'eval_on_test_set' / 'test_output.log') as f:
             content = f.read()
-            loss = float(
-                re.findall(r'\d+.\d+',
-                           re.findall(r'Eval:   \d+\.\d+', content)[0])[0])
-            self.assertEqual(loss, self.expected_loss)
+            loss = float(re.findall(r'\d+.\d+', re.findall(r'Eval: \d+\.\d+', content)[0])[0])
+            self.assertEqual(np.round(loss, 4), self.expected_loss)
         img_path = self.eval_output_path / 'eval_on_test_set' / 'images'
         list_all_imgs = list(img_path.glob('**/*.jpg'))
         self.assertEqual(len(list_all_imgs), self.expected_num_frames)
