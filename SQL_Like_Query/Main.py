71de7686cc410be8efa908a588ea97439c74ce19
hartmade <Dennis.Hartmann@student.uni-augsburg.de>
2019-09-20 12:36:19 +0200

    Optimized user experience, errors fixed, test_class optimized, new tests added, changed np.array to np.asarray for creation of HDF5DB-object

--- SQL_Like_Query/Main.py
+++ SQL_Like_Query/Main.py
@@ -15,7 +15,7 @@ def test():
     #      "/run/user/1001/gvfs/smb-share:server=137.250.170.56,share=home/s/t/stiebesi/data/RTM/Leoben/output/with_shapes/2019-07-24_16-32-40_5000p"
     # )
     test.load(
-        "/run/user/1001/gvfs/smb-share:server=137.250.170.56,share=share/cache/HDF5DB_Cache",
+        "/cfs/share/cache/HDF5DB_Cache",
         "Unrestricted"
     )
     # test.add_objects_from_path(
@@ -42,18 +42,26 @@ def test():
     # test.load("Data", "HDF5DB")
     # test.show_selection_options()
     # test.showObjects()
-    # test.select2("age", "2019-07-23_15-38-08", "=")
-    test.select("fvc_circle", 0.2, ">")
-    # test.select("output_frequency", 1, ">")
-    # test.select("fibre_content_runners", -0.7, "=")
-    # test.select("avg_level", 0.8, ">")
-    # test.select("posy_circle", 50, "<")
+
+    # TODO
+    # Min/Max width/heigth of rectangles
+    # Same for circle: radius 
+    # Total number of frames
+
+    # print(test.get_result_path_list()[0])
+    # test.select("age", "<", "2019-08-01_00-00-00")
+    # print(test.get_result_path_list()[0])
+    # test.select("fvc_circle", ">", 0.2)
+    # test.select("output_frequency", ">", 0.2)
+    # test.select("fibre_content_runners", "=", -0.7)
+    # test.select("avg_level", "<", 0.9)
+    test.select("posy_circle", "=", 18.375)
     # test.show_objects()
 
 
 # def main():
 t = timeit.Timer("test()", "from __main__ import test")
-print(t.timeit(1))
+print(str(t.timeit(1)) + "s")
 # cProfile.run('test()')
 
 # if __name__ == "__main__":
