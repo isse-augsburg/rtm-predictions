da7737ea781d00536f8b731ce7574a3d071543cf
schroeni <niklas.schroeter@student.uni-augsburg.de>
2019-11-12 10:06:24 +0100

    some linting errors fixed

--- Debugging/dry_spot_detection_leoben.py
+++ Debugging/dry_spot_detection_leoben.py
@@ -1,81 +1,69 @@
 import socket
 from pathlib import Path
 from time import time
-import threading
 import cv2
 import h5py
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 import numpy as np
-from PIL import Image
-from PIL import ImageDraw
 from multiprocessing import Pool
 import io
+
 
 def scale_coords_leoben(input_coords):
     scaled_coords = input_coords * 10
     return scaled_coords
 
 
-
 def __analyze_image(img, perm_map=None):
-   #
+    #
     _, threshold = cv2.threshold(img, 70, 190, cv2.THRESH_BINARY)
     _, contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    font = cv2.FONT_HERSHEY_COMPLEX
-    triangles, rectangles, pentagons, ellipses, circles = [], [], [], [], []
     min_size = 3
     dryspots = np.zeros_like(img, dtype=np.float)
     spots = False
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt, 0.005 * cv2.arcLength(cnt, True), True)
-        (x,y,w,h) = cv2.boundingRect(approx)
-        #cv2.rectangle(img, (x,y), (x+w,y+h), 100, 2)
-       
+        (x, y, w, h) = cv2.boundingRect(approx)
+        # cv2.rectangle(img, (x,y), (x+w,y+h), 100, 2)
+
         size = cv2.contourArea(cnt)
         if size < min_size:
             continue
         # max countour
         if size > 273440:
             continue
-       
+
         empty = np.zeros_like(img, dtype=np.float)
         cv2.fillPoly(empty, [np.squeeze(approx)], 255)
-        perm_cut = np.where(empty == 255, perm_map.astype(np.float), 0) # take values from perm map, where contour is
-        perm_cut = np.where((perm_cut <= 70) & (perm_cut >= 65), 0, perm_cut) #filter values of the background assuming background is between 70 and 65
-        perm_cut = np.where((perm_cut == 0), 0 , 255) # focus on anything other than background
-        avg_dryspot_prob = np.sum(perm_cut,dtype=np.float) / size # normalize with size of contour area
+        perm_cut = np.where(empty == 255, perm_map.astype(np.float), 0)  # take values from perm map, where contour is
+        perm_cut = np.where(
+            (perm_cut <= 70) & (perm_cut >= 65), 0, perm_cut
+        )  # filter values of the background assuming background is between 70 and 65
+        perm_cut = np.where((perm_cut == 0), 0, 255)  # focus on anything other than background
+        avg_dryspot_prob = np.sum(perm_cut, dtype=np.float) / size  # normalize with size of contour area
         # print(avg_dryspot_prob, np.sum(perm_cut,dtype=np.float), size) # debug print statement
         if avg_dryspot_prob > 250:
             cv2.fillPoly(dryspots, [np.squeeze(approx)], 255)
             spots = True
 
-
-
-        
-       
-   
     return spots, dryspots
-
-
 
 
 def dry_spot_analysis(file_path, output_dir):
     try:
-        f = h5py.File(file_path,'r')
-    except OSError as e:
+        f = h5py.File(file_path, "r")
+    except OSError:
         # print(file_path, "does not exist")
         return
-   
 
     t00 = time()
     use_orig_mesh = True
 
     coord_as_np_array = f["post/constant/entityresults/NODE/COORDINATE/ZONE1_set0/erfblock/res"][()]
-   
+
     output_dir.mkdir(exist_ok=True, parents=True)
-   
-    
+
     _all_coords = coord_as_np_array[:, :-1]
     scaled_coords = scale_coords_leoben(_all_coords)
     x = scaled_coords[:, 0]
@@ -83,7 +71,7 @@ def dry_spot_analysis(file_path, output_dir):
     spot_t = 0
 
     if use_orig_mesh:
-        triangles = f['/post/constant/connectivities/SHELL/erfblock/ic'][()]
+        triangles = f["/post/constant/connectivities/SHELL/erfblock/ic"][()]
         triangles = triangles - triangles.min()
         triangles = triangles[:, :-1]
         xi = np.linspace(0, 375, 376)
@@ -91,25 +79,20 @@ def dry_spot_analysis(file_path, output_dir):
         Xi, Yi = np.meshgrid(xi, yi)
         triang = tri.Triangulation(x, y, triangles=triangles)
 
-    keys = list(f['/post/singlestate'].keys())
-
-    start_detecting_changes_at = 5
-    changes, ups, downs = [], [], []
-    num_shapes = 0
+    keys = list(f["/post/singlestate"].keys())
 
     # Fiber fraction map creation with tripcolor
     fvc = f["/post/constant/entityresults/SHELL/FIBER_FRACTION/ZONE1_set1/erfblock/res"][()].flatten()
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    plt.tripcolor(triang, fvc, cmap='gray')
+    plt.tripcolor(triang, fvc, cmap="gray")
 
     ax.set(xlim=(0, 375), ylim=(0, 300))
-    plt.axis('off')
+    plt.axis("off")
     plt.tight_layout()
 
     extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
     plt.clim(0, 1)
-   
 
     perm_bytes = io.BytesIO()
     plt.savefig(perm_bytes, bbox_inches=extent)
@@ -118,13 +101,15 @@ def dry_spot_analysis(file_path, output_dir):
     file_bytes = np.asarray(bytearray(perm_bytes.read()), dtype=np.uint8)
     perm_map = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
 
-
-    #keys = [keys[111], keys[112], keys[113], keys[114], keys[115], keys[116], keys[117], keys[118], keys[119]]
+    # keys = [keys[111], keys[112], keys[113], keys[114], keys[115], keys[116], keys[117], keys[118], keys[119]]
     spot_list_s = []
     spot_list_e = []
     b_set = False
     for i, k in enumerate(keys):
-        data = f[f"/post/singlestate/{k}/entityresults/NODE/FILLING_FACTOR/ZONE1_set1/erfblock/res"][()]
+        try:
+            data = f[f"/post/singlestate/{k}/entityresults/NODE/FILLING_FACTOR/ZONE1_set1/erfblock/res"][()]
+        except KeyError:
+            continue
         z = data.flatten()
 
         fig = plt.figure()
@@ -133,12 +118,12 @@ def dry_spot_analysis(file_path, output_dir):
         if use_orig_mesh:
             interpolator = tri.LinearTriInterpolator(triang, z)
             zi = interpolator(Xi, Yi)
-            ax2.contourf(xi, yi, zi, levels=10, cmap='gray', extend='both')
-           
+            ax2.contourf(xi, yi, zi, levels=10, cmap="gray", extend="both")
+
         else:
-            ax2.tricontourf(x, y, z, levels=10, colors='k')
+            ax2.tricontourf(x, y, z, levels=10, colors="k")
         ax2.set(xlim=(0, 375), ylim=(0, 300))
-        plt.axis('off')
+        plt.axis("off")
         plt.tight_layout()
 
         extent = ax2.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
@@ -148,48 +133,45 @@ def dry_spot_analysis(file_path, output_dir):
         bytes_tmp.seek(0)
         file_bytes = np.asarray(bytearray(bytes_tmp.read()), dtype=np.uint8)
         img = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
-        
-      
-       
-       
-       
+
         img = 255 - img
         spot_b, dryspot_img = __analyze_image(img, perm_map)
-        if (spot_b):
-            if(i+1 != spot_t + 1):
-                spot_list_s.append(i+1)
+        if spot_b:
+            if i + 1 != spot_t + 1:
+                spot_list_s.append(i + 1)
                 b_set = True
-            spot_t = i+1
-            
-        elif(b_set):
-            b_set = False
-            spot_list_e.append(i+1)
+            spot_t = i + 1
 
-        cv2.imwrite(str(output_dir / ('%d_dry.png' % (i + 1))), dryspot_img)
-       
-    if(len(spot_list_e) < len(spot_list_s)):
-        spot_list_e.append('end')
+        elif b_set:
+            b_set = False
+            spot_list_e.append(i + 1)
+
+        cv2.imwrite(str(output_dir / ("%d_dry.png" % (i + 1))), dryspot_img)
+
+    if len(spot_list_e) < len(spot_list_s):
+        spot_list_e.append("end")
     print(
-        f'{output_dir} Overall time: {time() - t00}. Remember: Indeces in PAM RTM start with 1, images have pam-like indices. '
+        f"{output_dir} Overall time: {time() - t00}. Remember: arrays start at one."
         f'Dryspots at: {[f"{one} - {two}" for (one,two) in zip(spot_list_s,spot_list_e)]}'
-        )
+    )
+
 
 def multiprocess_wrapper(i):
-   # dry_spot_analysis(source / str(i) / str('2019-07-23_15-38-08_%d_RESULT.erfh5' % i), Path('/home/schroeter/Desktop/2019-07-23_15-38-08_5000p') / str(i))
-   dry_spot_analysis(source / str(i) / str('2019-07-23_15-38-08_%d_RESULT.erfh5' % i), Path('/cfs/share/cache/DrySpotDet/2019-07-23_15-38-08_5000p') / str(i))
+    dry_spot_analysis(
+        source / str(i) / str("2019-07-23_15-38-08_%d_RESULT.erfh5" % i),
+        Path("/cfs/share/cache/DrySpotDet/2019-07-23_15-38-08_5000p") / str(i),
+    )
+
 
 if __name__ == "__main__":
     if socket.gethostname() == "swtse130":
-        source = Path(r'X:\s\t\stiebesi\data\RTM\Leoben\output\with_shapes\2019-07-23_15-38-08_5000p')
+        source = Path(r"X:\s\t\stiebesi\data\RTM\Leoben\output\with_shapes\2019-07-23_15-38-08_5000p")
     else:
-        source = Path('/run/user/1001/gvfs/smb-share:server=137.250.170.56,share=home/s/t/stiebesi/data/RTM/Leoben/output/with_shapes/2019-07-23_15-38-08_5000p')
-        source = Path('/cfs/home/s/t/stiebesi/data/RTM/Leoben/output/with_shapes/2019-07-23_15-38-08_5000p')
+        source = Path(
+            "/run/user/1001/gvfs/smb-share:server=137.250.170.56,share=home/s/t/stiebesi/data/RTM/"
+            "Leoben/output/with_shapes/2019-07-23_15-38-08_5000p"
+        )
+        source = Path("/cfs/home/s/t/stiebesi/data/RTM/Leoben/output/with_shapes/2019-07-23_15-38-08_5000p")
 
-
-   # for i in range(0, 11):
-   #     dry_spot_analysis(source / str(i) / str('2019-07-23_15-38-08_%d_RESULT.erfh5' % i), Path('/home/schroeter/Desktop/2019-07-23_15-38-08_5000p') / str(i))
-    
-   
-    with Pool()as p:
+    with Pool() as p:
         p.map(multiprocess_wrapper, range(0, 5000))
-       
--- Pipeline/data_loaders_IMG.py
+++ Pipeline/data_loaders_IMG.py
@@ -1,19 +1,14 @@
 import logging
+
 # import random
 from functools import partial
-from pathlib import Path
 
 import h5py
 import numpy as np
 
 # from Pipeline.data_gather import get_filelist_within_folder
 # data_function must return [(data, label) ... (data, label)]
-from plots_and_images import (
-    draw_polygon_map,
-    plot_wrapper,
-    scale_coords_lautern,
-    scale_coords_leoben
-)
+from plots_and_images import draw_polygon_map, plot_wrapper, scale_coords_leoben
 
 
 # from PIL import Image
@@ -94,31 +89,24 @@ def create_np_image(target_shape=(143, 111), norm_coords=None, data=None):
     coords_value[:, 1] = coords_value[:, 1] * (target_shape[1] - 1)
     coords_value[:, 2] = coords_value[:, 2]
     # coords_value = coords_value.astype(np.int)
-    arr[
-        coords_value[:, 0].astype(np.int), coords_value[:, 1].astype(np.int)
-    ] = coords_value[:, 2]
+    arr[coords_value[:, 0].astype(np.int), coords_value[:, 1].astype(np.int)] = coords_value[:, 2]
 
     return arr
 
 
-def get_images_of_flow_front_and_permeability_map(
-        filename, wanted_num=10, imsize=(155, 155)
-):
+def get_images_of_flow_front_and_permeability_map(filename, wanted_num=10, imsize=(155, 155)):
     f = h5py.File(filename, "r")
     im, scaled_coords, triangle_coords = get_local_properties_map(f, imsize)
 
     all_states = list(f["post"]["singlestate"].keys())
-    selected_states = get_fixed_number_of_elements_and_indices(
-        all_states, wanted_num=wanted_num
-    )
+    selected_states = get_fixed_number_of_elements_and_indices(all_states, wanted_num=wanted_num)
 
     fillings = []
     for i, state in enumerate(all_states):
         try:
-            filling_factor = \
-                f["post"]["singlestate"][state]["entityresults"]["NODE"][
-                    "FILLING_FACTOR"
-                ]["ZONE1_set1"]["erfblock"]["res"][()]
+            filling_factor = f["post"]["singlestate"][state]["entityresults"]["NODE"]["FILLING_FACTOR"]["ZONE1_set1"][
+                "erfblock"
+            ]["res"][()]
         except KeyError:
             return None
         fillings.append(filling_factor)
@@ -126,8 +114,7 @@ def get_images_of_flow_front_and_permeability_map(
     # indices = [int(x.split('state')[1]) for x in selected_states]
     indices = selected_states
     label = np.asarray(im)
-    wrapper = partial(plot_wrapper, triangle_coords, scaled_coords, fillings,
-                      imsize)
+    wrapper = partial(plot_wrapper, triangle_coords, scaled_coords, fillings, imsize)
     res = []
     for i in indices:
         try:
@@ -150,9 +137,7 @@ def get_images_of_flow_front_and_permeability_map(
     return [(img_stack[0:wanted_num], label)]
 
 
-def get_fixed_number_of_elements_and_indices(
-        input_list, wanted_num
-):
+def get_fixed_number_of_elements_and_indices(input_list, wanted_num):
     num = len(input_list)
     dist = num / wanted_num
     if num == wanted_num:
@@ -169,31 +154,25 @@ def get_fixed_number_of_elements_and_indices(
 
 
 def get_local_properties_map(f, imsize):
-    coord_as_np_array = f[
-        "post/constant/entityresults/NODE/COORDINATE/ZONE1_set0/erfblock/res"
-    ][()]
+    coord_as_np_array = f["post/constant/entityresults/NODE/COORDINATE/ZONE1_set0/erfblock/res"][()]
     _all_coords = coord_as_np_array[:, :-1]
-    
+
     scaled_coords = scale_coords_leoben(_all_coords)
     # norm_cords = normalize_coords(_all_coords)
     triangle_coords = f["post/constant/connectivities/SHELL/erfblock/ic"][()]
-    triangle_coords = triangle_coords[:, :-1] - 151980 # required for Leoben data
+    triangle_coords = triangle_coords[:, :-1] - 151980  # required for Leoben data
 
     data = f["post/constant/entityresults/SHELL/"]
 
-    im = create_local_properties_map(
-        data, scaled_coords, triangle_coords, "FIBER_FRACTION"
-    )
+    im = create_local_properties_map(data, scaled_coords, triangle_coords, "FIBER_FRACTION")
     if im.size != imsize:
         im = im.resize(imsize)
     return im, scaled_coords, triangle_coords
 
 
-def create_local_properties_map(
-        data, scaled_coords, triangle_coords, _type="FIBER_FRACTION"
-):
+def create_local_properties_map(data, scaled_coords, triangle_coords, _type="FIBER_FRACTION"):
     values_for_triangles = data[_type]["ZONE1_set1"]["erfblock"]["res"][()]
-    im = draw_polygon_map(values_for_triangles, scaled_coords, triangle_coords,size=(152*3,120*3))
+    im = draw_polygon_map(values_for_triangles, scaled_coords, triangle_coords, size=(152 * 3, 120 * 3))
     return im
 
 
@@ -217,29 +196,23 @@ def get_sensordata_and_flowfront(file, target_shape=(38, 30)):
     f = h5py.File(file, "r")
     instances = []
     try:
-        coord_as_np_array = f[
-            "post/constant/entityresults/NODE/COORDINATE/ZONE1_set0/" 
-            "erfblock/res"
-        ][()]
+        coord_as_np_array = f["post/constant/entityresults/NODE/COORDINATE/ZONE1_set0/" "erfblock/res"][()]
         # Cut off last column (z), since it is filled with 1s anyway
         _coords = coord_as_np_array[:, :-1]
 
         _coords = normalize_coords(_coords)
 
-        pressure_array = \
-            f["post"]["multistate"]["TIMESERIES1"]["multientityresults"][
-                "SENSOR"]["PRESSURE"]["ZONE1_set1"]["erfblock"]["res"][()]
+        pressure_array = f["post"]["multistate"]["TIMESERIES1"]["multientityresults"]["SENSOR"]["PRESSURE"][
+            "ZONE1_set1"
+        ]["erfblock"]["res"][()]
         # convert barye to bar ( smaller values are more stable while training)
-        pressure_array = (
-            pressure_array / 100000
-        )
+        pressure_array = pressure_array / 100000
         all_states = f["post"]["singlestate"]
 
         filling_factors_at_certain_times = [
-            f["post"]["singlestate"][state]["entityresults"]["NODE"][
-                "FILLING_FACTOR"][
-                "ZONE1_set1"
-            ]["erfblock"]["res"][()]
+            f["post"]["singlestate"][state]["entityresults"]["NODE"]["FILLING_FACTOR"]["ZONE1_set1"]["erfblock"]["res"][
+                ()
+            ]
             for state in all_states
         ]
         flat_fillings = np.squeeze(filling_factors_at_certain_times)
@@ -251,15 +224,14 @@ def get_sensordata_and_flowfront(file, target_shape=(38, 30)):
             s = state.replace("state", "")
             state_num = int(s)
             sensordata = np.squeeze(pressure_array[state_num - 1])
-            arr = create_np_image(
-                target_shape=target_shape, norm_coords=_coords, data=filling
-            )
+            arr = create_np_image(target_shape=target_shape, norm_coords=_coords, data=filling)
             instances.append((sensordata, arr))
         except IndexError:
             continue
     if len(instances) == 0:
         return None
     return instances
+
 
 # FIXME also here, load image is used. where is it?
 # def get_image_percentage(folder):
@@ -279,8 +251,8 @@ def get_sensordata_and_flowfront(file, target_shape=(38, 30)):
 
 
 if __name__ == "__main__":
-    f = h5py.File('/home/schroeter/Desktop/2019-07-23_15-38-08_7_RESULT.erfh5', 'r')
-    im, scaled_coords, triangle_coords = get_local_properties_map(f, (152*3,120*3))
+    f = h5py.File("/home/schroeter/Desktop/2019-07-23_15-38-08_7_RESULT.erfh5", "r")
+    im, scaled_coords, triangle_coords = get_local_properties_map(f, (152 * 3, 120 * 3))
     im.show()
 
     # get_sensordata_and_flowfront(
