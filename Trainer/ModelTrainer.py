5779e4a3d6b9f36c43497935aa934c1f43b9ef47
Simon Stieber <simon.stieber@informatik.uni-augsburg.de>
2020-03-04 17:06:04 +0100

    Fixed tests.

--- Trainer/ModelTrainer.py
+++ Trainer/ModelTrainer.py
@@ -221,34 +221,53 @@ class ModelTrainer:
         dl_str = '  \n'.join([f"{k}: {dl_info[k]}" for k in dl_info if dl_info[k] is not None])
         self.writer.add_text("Data/DataLoader", f"{dl_str}")
 
+
     def __create_model_and_optimizer(self):
         logger = logging.getLogger(__name__)
         logger.info("Generating Model")
-        if self.model is None:
-            self.model = self.model_creation_function()
-            self.model_name = self.model.__class__.__name__
+        if not self.use_mixed_precision:
+            if self.model is None:
+                self.model = self.model_creation_function()
+                self.model_name = self.model.__class__.__name__
 
-        if self.optimizer is None:
-            if self.optimizer_path is None:
-                self.optimizer = self.optimizer_function(self.model.parameters())
-            else:
-                self.logger.info(f'Loading optimizer state from {self.optimizer_path}')
-                self.optimizer = self.optimizer_function(self.model.parameters())
-                checkpoint = torch.load(self.optimizer_path)
-                self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+                if "swt-dgx" in socket.gethostname():
+                    logger.info("Invoking data parallel model.")
+                    self.model = nn.DataParallel(self.model).to("cuda:0")
+                else:
+                    self.model = self.model.to("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        if self.lr_scheduler_function is not None:
-            self.lr_scheduler = self.lr_scheduler_function(self.optimizer)
+            if self.optimizer is None:
+                if self.optimizer_path is None:
+                    self.optimizer = self.optimizer_function(self.model.parameters())
+                else:
+                    self.logger.info(f'Loading optimizer state from {self.optimizer_path}')
+                    self.optimizer = self.optimizer_function(self.model.parameters())
+                    checkpoint = torch.load(self.optimizer_path)
+                    self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-        if self.use_mixed_precision:
+            if self.lr_scheduler_function is not None:
+                self.lr_scheduler = self.lr_scheduler_function(self.optimizer)
+        else:
+            if self.model is None:
+                self.model = self.model_creation_function()
+                self.model_name = self.model.__class__.__name__
+
+            if self.optimizer is None:
+                if self.optimizer_path is None:
+                    self.optimizer = self.optimizer_function(self.model.parameters())
+                else:
+                    self.logger.info(f'Loading optimizer state from {self.optimizer_path}')
+                    self.optimizer = self.optimizer_function(self.model.parameters())
+                    checkpoint = torch.load(self.optimizer_path)
+                    self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+            if self.lr_scheduler_function is not None:
+                self.lr_scheduler = self.lr_scheduler_function(self.optimizer)
             self.model = self.model.cuda()
             self.model, self.optimizer = amp.initialize(self.model, self.optimizer, opt_level="O1")
-
-        if "swt-dgx" in socket.gethostname():
-            logger.info("Invoking data parallel model.")
-            self.model = nn.DataParallel(self.model).to("cuda:0")
-        else:
-            self.model = self.model.to("cuda:0" if torch.cuda.is_available() else "cpu")
+            if "swt-dgx" in socket.gethostname():
+                logger.info("Invoking data parallel model.")
+                self.model = nn.DataParallel(self.model).to("cuda:0")
 
     def start_training(self,):
         """ Sets up training and logging and starts train loop
