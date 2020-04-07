f289417aa7df7c49fa97376a22e3b396a661e1c5
may <johannes.may@udo.edu>
2020-04-07 19:30:34 +0200

    try johanmay directory

--- Resources/testing.py
+++ Resources/testing.py
@@ -13,7 +13,7 @@ torch_datasets = test_src_dir / "TestSaveDatasetsTorch" / "unix"
 # if running_in_docker():
 #     test_out_dir = Path('/cache')
 # else:
-test_out_dir = Path(f'/cfs/share/cache/output_{getpass.getuser()}/tests')
+test_out_dir = Path(f'/cfs/share/cache/output_johanmay/tests')
 
 if os.name == "nt":
     test_out_dir = Path(r'C:\Users\stiebesi\CACHE\test_output')
