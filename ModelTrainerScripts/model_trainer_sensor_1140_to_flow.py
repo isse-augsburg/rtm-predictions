67d19a9d0beec03a34d3dbe2e991c994cdd32917
Simon Stieber <simon.stieber@informatik.uni-augsburg.de>
2020-04-08 21:07:03 +0200

    Start of demo mode.

--- ModelTrainerScripts/model_trainer_sensor_1140_to_flow.py
+++ ModelTrainerScripts/model_trainer_sensor_1140_to_flow.py
@@ -36,7 +36,8 @@ if __name__ == "__main__":
         optimizer_function=lambda params: torch.optim.AdamW(params, lr=0.0001),
         classification_evaluator_function=lambda summary_writer:
         SensorToFlowfrontEvaluator(summary_writer=summary_writer),
-        demo_path=args.demo
+        demo_path=args.demo,
+        run_eval_step_before_training=True
     )
 
     if not args.eval:
